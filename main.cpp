bb6074f8e01bd3e70ef20966dd74edd1357384a1
Jonathan <joni7840850@gmail.com>
2023-02-19 18:58:23 -0600

    Add main file

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "Piece.h"
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(400, 400), "Chessboard");
+    Piece pawn(0, 0, "pawn", "white", "images");
+    Piece rook(50, 0, "rook", "white", "images");
+    pawn.draw_piece(window);
+    rook.draw_piece(window);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.display();
+        sf::sleep(sf::milliseconds(50)); 
+    }
+
+    return 0;
+}
