4ee05ebaec6a8dfb9309c5aa9fa83073bff35cbf
Jonathan <joni7840850@gmail.com>
2023-02-20 10:33:10 -0600

    Add main program

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "Piece.h"
+#include "Board.h"
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(400, 400), "Chessboard");
     Piece pawn(0, 0, "pawn", "white", "images");
     Piece rook(50, 0, "rook", "white", "images");
+    Board board(400, 400);
+    board.draw_board(window);
     pawn.draw_piece(window);
     rook.draw_piece(window);
 
