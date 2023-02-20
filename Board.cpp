bc1b9c41aa1b834ae2e4af079ce0b76c346f2cbd
Jonathan <joni7840850@gmail.com>
2023-02-20 10:32:57 -0600

    Add separate files for the board

--- Board.cpp
+++ Board.cpp
@@ -1,26 +1,12 @@
-#include <SFML/Graphics.hpp>
+#include "Board.h"
 
-/*
-* Board class
-* This class is responsible for drawing the chessboard
-* and the pieces on the board.
-* The board is a 8x8 grid of squares.
-*/
+Board::Board(int widht, int height)
+    : m_widht {widht}, m_height {height}
+    {
 
-class Board
-{
-private:
-    int m_widht;
-    int m_height;
+    }
 
-public:
-    Board(int widht = 400, int height = 400)
-        : m_widht {widht}, m_height {height}
-        {
-
-        }
-
-    void draw_board(sf::RenderWindow& window){
+void Board::draw_board(sf::RenderWindow& window){
         sf::RectangleShape square(sf::Vector2f(50, 50));
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
@@ -36,23 +22,3 @@ public:
                 }
             }
         }
-};
-
-int main() {
-    sf::RenderWindow window(sf::VideoMode(400, 400), "Chessboard");
-    Board board;
-    board.draw_board(window);
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.display();
-        sf::sleep(sf::milliseconds(50)); 
-    }
-
-    return 0;
-}
