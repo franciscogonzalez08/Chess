48109cda13a8dc97e1c786f1fb2f0ffdba3d74d5
Jonathan <joni7840850@gmail.com>
2023-03-18 16:25:02 -0600

    Add command to compile validator class

--- run_chess.sh
+++ run_chess.sh
@@ -1,5 +1,6 @@
 g++ -c main.cpp
 g++ -c Piece.cpp
 g++ -c Board.cpp
-g++ -o main_app main.o Piece.o Board.o -lsfml-graphics -lsfml-window -lsfml-system
+g++ -c Validator.cpp
+g++ -o main_app main.o Piece.o Board.o Validator.o -lsfml-graphics -lsfml-window -lsfml-system
 ./main_app   
