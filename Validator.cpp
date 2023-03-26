fa8206b17bf0bce7baa13f3412d7105d33866071
Jonathan <joni7840850@gmail.com>
2023-03-26 09:45:28 -0600

    Move some method to make code more readable

--- Validator.cpp
+++ Validator.cpp
@@ -121,7 +121,7 @@ std::vector < std::pair < int, int >> Validator::compute_possible_moves(std::str
         possible_moves = assing_king_moves(x, y, pieces_info, piece_color);
         return possible_moves;
     }
-    //Here is a warning because this may return nothing, not sure if this is a problem
+    //Here is a warning because this may return nothing, not a problem since this function is called only when a piece is selected.
 }
 
 std::vector < std::pair < int, int >> Validator::assing_pawn_moves(int x_postion, int y_position, std::vector < std::tuple < int, int, std::string >> pieces_info, std::string piece_color) {
@@ -215,6 +215,26 @@ std::vector < std::pair < int, int >> Validator::assing_knight_moves(int x_posti
     possible_moves.push_back(add_position(std::make_pair(x_postion, y_position), std::make_pair(2, -1)));
     possible_moves.push_back(add_position(std::make_pair(x_postion, y_position), std::make_pair(-2, 1)));
     possible_moves.push_back(add_position(std::make_pair(x_postion, y_position), std::make_pair(-2, -1)));
+    return possible_moves;
+}
+
+std::vector < std::pair < int, int >> Validator::assing_rook_moves(int x_postion, int y_position, std::vector < std::tuple < int, int, std::string >> pieces_info, std::string piece_color) {
+    /*
+    This function computes the possible moves of the rook.
+    Arguments:
+        x_postion: x position of the rook
+        y_position: y position of the rook
+        pieces_info: vector with the pieces of the board
+    Returns:
+        a vector with the possible moves of the rook
+    */
+    std::vector < std::pair < int, int >> possible_moves;
+    std::pair < int, int > possible_move;
+    //This looks like can be done in another method
+    possible_moves = stack_moves(x_postion, y_position, 1, 8, pieces_info, possible_moves, piece_color);
+    possible_moves = stack_moves(x_postion, y_position, 3, 8, pieces_info, possible_moves, piece_color);
+    possible_moves = stack_moves(x_postion, y_position, 5, 8, pieces_info, possible_moves, piece_color);
+    possible_moves = stack_moves(x_postion, y_position, 7, 8, pieces_info, possible_moves, piece_color);
     return possible_moves;
 }
 
@@ -401,26 +421,6 @@ int Validator::check_colision(std::pair < int, int > new_move, std::vector < std
         }
     }
     return 0;
-}
-
-std::vector < std::pair < int, int >> Validator::assing_rook_moves(int x_postion, int y_position, std::vector < std::tuple < int, int, std::string >> pieces_info, std::string piece_color) {
-    /*
-    This function computes the possible moves of the rook.
-    Arguments:
-        x_postion: x position of the rook
-        y_position: y position of the rook
-        pieces_info: vector with the pieces of the board
-    Returns:
-        a vector with the possible moves of the rook
-    */
-    std::vector < std::pair < int, int >> possible_moves;
-    std::pair < int, int > possible_move;
-    //This looks like can be done in another method
-    possible_moves = stack_moves(x_postion, y_position, 1, 8, pieces_info, possible_moves, piece_color);
-    possible_moves = stack_moves(x_postion, y_position, 3, 8, pieces_info, possible_moves, piece_color);
-    possible_moves = stack_moves(x_postion, y_position, 5, 8, pieces_info, possible_moves, piece_color);
-    possible_moves = stack_moves(x_postion, y_position, 7, 8, pieces_info, possible_moves, piece_color);
-    return possible_moves;
 }
 
 void Validator::draw_possible_moves(sf::RenderWindow & window, std::vector < std::pair < int, int >> possible_moves) {
--- Validator.h
+++ Validator.h
@@ -20,22 +20,21 @@
 class Validator {
     private: std::pair < int,
     int > board_dims;
+    int check_colision(std::pair<int,int> new_move, std::vector<std::tuple<int, int,std::string>> pieces_info, std::string piece_color);
     std::string transform_name(std::string piece_name);
-    void draw_square(sf::RenderWindow& window, int x, int y);
     std::pair<int,int> add_position(std::pair<int,int> piece_possition, std::pair<int,int> new_position);
     std::pair<int,int> compute_square_width_height();
+    std::vector<std::pair<int,int>> compute_possible_moves(std::string piece_name, int x, int y, std::vector<std::tuple<int, int,std::string>> pieces_info, std::string piece_color);
     std::vector<std::pair<int,int>> assing_pawn_moves(int x_postion, int y_position, std::vector<std::tuple<int, int,std::string>> pieces_info, std::string piece_color);
     std::vector<std::pair<int,int>> assing_knight_moves(int x_postion, int y_position, std::vector<std::tuple<int, int,std::string>> pieces_info, std::string piece_color);
     std::vector<std::pair<int,int>> assing_bishop_moves(int x_postion, int y_position, std::vector<std::tuple<int, int,std::string>> pieces_info, std::string piece_color);
     std::vector<std::pair<int,int>> assing_queen_moves(int x_postion, int y_position, std::vector<std::tuple<int, int,std::string>> pieces_info, std::string piece_color);
     std::vector<std::pair<int,int>> assing_king_moves(int x_postion, int y_position, std::vector<std::tuple<int, int,std::string>> pieces_info, std::string piece_color);
-    std::vector<std::pair<int,int>> compute_possible_moves(std::string piece_name, int x, int y, std::vector<std::tuple<int, int,std::string>> pieces_info, std::string piece_color);
-    void draw_possible_moves(sf::RenderWindow & window, std::vector<std::pair<int,int>> possible_moves);
-    int check_colision(std::pair<int,int> new_move, std::vector<std::tuple<int, int,std::string>> pieces_info, std::string piece_color);
     std::vector<std::pair<int,int>> assing_rook_moves(int x_postion, int y_position, std::vector<std::tuple<int, int,std::string>> pieces_info, std::string piece_color);
     std::vector<std::pair<int,int>> stack_moves(int x_postion, int y_position, int direction, int number_of_moves, 
     std::vector<std::tuple<int, int,std::string>> pieces_info, std::vector<std::pair<int,int>> original_possible_moves, std::string piece_color);
-
+    void draw_square(sf::RenderWindow& window, int x, int y);
+    void draw_possible_moves(sf::RenderWindow & window, std::vector<std::pair<int,int>> possible_moves);
 
     public: Validator(int x, int y);
     std::vector<std::pair<int,int>> show_possible_moves(sf::RenderWindow& window, std::string piece_name, std::string piece_color, int piece_x_position, int piece_y_position,
